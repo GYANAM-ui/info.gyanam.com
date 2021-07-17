443607948f7c9a28cefb79fb9ef8f96166f8dc4e
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-07-17 18:06:03 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,59 @@
+link1_URL = "https://gyanam-ui.github.io/AI-Object-Finder/";
+link2_URL = "https://gyanam-ui.github.io/AI-Video-Surveillance/";
+link3_URL = "https://gyanam-ui.github.io/AI-BABY-MONITORING/";
+link4_URL = "https://gyanam-ui.github.io/AI-MUSIC/";
+link5_URL = "https://gyanam-ui.github.io/emotion-to-emoji/";
+
+// Projects function links 
+function link1() {
+    window.open(link1_URL);
+}
+
+function link2() {
+    window.open(link2_URL);
+}
+
+function link3() {
+    window.open(link3_URL);
+}
+
+function link4() {
+    window.open(link4_URL);
+}
+
+function link5() {
+    window.open(link5_URL);
+}
+
+// Social Links 
+function facebook() {
+    window.open("https://facebook.com/ray.divoire");
+}
+
+function instagram() {
+    window.open("https://www.instagram.com/rayhiphop_");
+}
+
+function youtube() {
+    window.open("https://www.youtube.com/channel/UCgM63NRv7LBnGDgUWO2fjaQ");
+}
+
+function pinterest() {
+    window.open("https://pinterest.com/ggyanam869/");
+}
+
+function snapchat() {
+    window.open("https://www.snapchat.com/add/ray_dcruz");
+}
+
+function email() {
+    window.open("mailto:nestcoderlive@gmail.com");
+}
+
+function telegram() {
+    window.open("https://t.me/raydcruz");
+}
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
